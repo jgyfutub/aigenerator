7cf8681e08dfb2379bbcff6deefda04f0348cd6f
Vedant Pandey <vedantpandey459@gmail.com>
2023-07-06 22:47:08 +0530

    image generated

--- front/src/Pages/AiGenerator.js
+++ front/src/Pages/AiGenerator.js
@@ -4,7 +4,7 @@ import { ReactDOM } from "react";
 import axios from 'axios';
 export default function Aigenerator(){
 const [images,setimages]=useState([])
-
+const [imageSrc, setImageSrc] = useState('')
 const handleImageInput=(event)=>{
     const file=event.target.files[0]
     setimages([...images,file])
@@ -17,19 +17,23 @@ const handleSubmit=async(e)=>{
     formdata.append('image2',images[1])
     console.log(formdata)
     const senddata=await axios.post('http://127.0.0.1:8000/neuralstyletransfer/',formdata)
-    const imgai=document.getElementsByClassName('imgai')
-    imgai.src='data:image/png;base64,'+senddata.data.image
-    console.log(imgai.src)
+    let url='data:image/png;base64,'+senddata.data.image
+    setImageSrc(url)
+    console.log(imageSrc)
+    changeImage(url)
+    console.log(imageSrc)
 }
-
+const changeImage = (url) => {
+    setImageSrc(url)
+  };
 return(
     <div>
     <form onSubmit={handleSubmit}>
     <input type='file' onChange={handleImageInput}/>
     <input type='file' onChange={handleImageInput}/>
     <button type='submit'>Submit</button>
-    <img src="" className="imgai" alt="Generated image by nerual style"/>
     </form>
+    <img src={imageSrc} className="imgai" alt="Generated image by nerual style" height={100} width={100}/>
     </div>
 )
 }
--- pybackend/myproject/myapp/views.py
+++ pybackend/myproject/myapp/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from django.http import HttpResponse
 from django.http import JsonResponse
 import json
+import os
 import base64
 from PIL import Image
 import numpy as np
@@ -33,12 +34,14 @@ class ReactView(APIView):
         if len(gen_arr.shape)>3:
             gen_arr=tf.squeeze(gen_arr,axis=0)
         array = tf.keras.preprocessing.image.img_to_array(gen_arr)
+        array=array*255.0
         array=tf.cast(array,tf.uint8)
+        print(array)
         array=array.numpy()
         imagegenerated = Image.fromarray(array)
-        imagegenerated.save('C:/Users/Acer/OneDrive/Desktop/imagegenerator/pybackend/myproject/media/images.png','PNG' )
+        imagegenerated.save('C:/Users/Acer/OneDrive/Desktop/imagegenerator/pybackend/myproject/media/images'+str(len(os.listdir('C:/Users/Acer/OneDrive/Desktop/imagegenerator/pybackend/myproject/media'))+1)+'.png','PNG' )
         imagegenerated_bytes=imagegenerated.tobytes()
         imagegenerated_base64=base64.b64encode(imagegenerated_bytes).decode('utf-8')
-        return JsonResponse({'message':'django connected!!','image':imagegenerated_base64})
+        return JsonResponse({'message':'django connected!!','image':imagegenerated_base64,'imageurl':'C:/Users/Acer/OneDrive/Desktop/imagegenerator/pybackend/myproject/media/images'+str(len(os.listdir('C:/Users/Acer/OneDrive/Desktop/imagegenerator/pybackend/myproject/media')))+'.png'})
     def get(self,request):
         return JsonResponse({'message':'neural style transfer api'})
