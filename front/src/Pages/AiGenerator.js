aeba85a2fd4308c8a2335b9b1ae34852bbcba25b
Vedant Pandey <vedantpandey459@gmail.com>
2023-07-06 14:59:31 +0530

    trying django

--- front/src/Pages/AiGenerator.js
+++ front/src/Pages/AiGenerator.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
-// import { loadGraphModel } from '@tensorflow/tfjs-converter';
 import * as tf from "@tensorflow/tfjs";
 import { ReactDOM } from "react";
 import axios from 'axios';
 export default function Aigenerator(){
 const [images,setimages]=useState([])
 
-const load_img=async(image)=>{
-    console.log(image)
-    const imgfloattensor=await tf.cast(image,'float32')
-    const resizedimage=tf.image.resizeBilinear(imgfloattensor,[512,512])
-    console.log(resizedimage)
-    return resizedimage
-}
+// const load_img=async(image)=>{
+//     console.log(image)
+//     const imgfloattensor=await tf.cast(image,'float32')
+//     const resizedimage=tf.image.resizeBilinear(imgfloattensor,[512,512])
+//     console.log(resizedimage)
+//     return resizedimage
+// }
 const handleImageInput=(event)=>{
     const file=event.target.files[0]
     const reader=new FileReader()
@@ -21,7 +20,7 @@ const handleImageInput=(event)=>{
         const image=new Image()
         image.src=dataUrl
         image.onload=()=>{
-            setimages([...images,image])
+            setimages([...images,tf.browser.fromPixels(image)])
         }
     }
     if(file){
@@ -29,21 +28,10 @@ const handleImageInput=(event)=>{
     }
     console.log(images)
 }
-const handleSubmit=(e)=>{
+const handleSubmit=async(e)=>{
     e.preventDefault()
-    console.log(tf.version)
-    model()
-}
-const model=async()=>{
-    // const model= await tf.loadGraphModel('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2',{fromTFHub:true,mode:'no-cors'})
-    // const model1=await axios.get('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2', {mode: 'no-cors'})
-    // console.log(model1)
-// const zeros = tf.zeros([1, 224, 224, 3]);
-// model.predict(zeros).print();
-    // const styled_image= model.predict(load_img(images[0]))
-    const modeltransformer= await tf.loadGraphModel('/Styletransfertfjs/model.json')
-    // const transformer_image= modeltransformer.execute([load_img(images[0]),load_img(images[1])])
-    // console.log(transformer_image)
+    const senddata=await axios.post('http://127.0.0.1:8000/neuralstyletransfer',{array:images})
+    console.log(senddata)
 }
 
 return(
