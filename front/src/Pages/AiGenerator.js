a58b19640f20befbe9791feb2fcb41808a7ee507
Vedant Pandey <vedantpandey459@gmail.com>
2023-07-07 22:21:06 +0530

    frontend modified

--- front/src/Pages/AiGenerator.js
+++ front/src/Pages/AiGenerator.js
@@ -4,6 +4,7 @@ import { ReactDOM } from "react";
 import axios from 'axios';
 import './pages.css';
 import genimages from './media/images.png';
+import Header from "./Header";
 export default function Aigenerator(){
 const [images,setimages]=useState([])
 const [imageSrc, setImageSrc] = useState('')
@@ -25,15 +26,28 @@ const handleSubmit=async(e)=>{
     console.log(imageSrc)
 }
 return(
+    <div>
+    <Header/>
     <div className="aidiv">
     <form onSubmit={handleSubmit}>
     <div>
+    <div style={{display:'grid'}}>
+    <p>choose image</p>
+    <input type='file' onChange={handleImageInput}/></div>
+    <div style={{display:'grid'}}>
+    <p>choose styling image</p>
     <input type='file' onChange={handleImageInput}/>
-    <input type='file' onChange={handleImageInput}/>
+    </div>
     </div>
     <button type='submit'>Submit</button>
     </form>
     <img src={genimages} alt="Generated image by nerual style"/>
+    <div style={{display:'flex',justifyContent:'center',columnGap:'10px'}}>
+    <p>Save Image?</p>
+    <button>yes</button>
+    <button>no</button>
+    </div>
+    </div>
     </div>
 )
 }
---
+++ front/src/Pages/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './pages.css';
+export default function Header(){
+    return(
+        <div className="Header">
+            <button>≡</button>
+            <p>AiGenerator</p>
+        </div>
+    )
+}
