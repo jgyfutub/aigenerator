a4118e503f7eac35330bb7d4ddbf1a19f8d4a066
Vedant Pandey <vedantpandey459@gmail.com>
2023-07-07 23:38:37 +0530

    pop up added

--- front/src/Pages/Header.js
+++ front/src/Pages/Header.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React ,{useState,useEffect}from "react";
 import './pages.css';
 export default function Header(){
+    const [bool,funcbool]=useState(false)
+    const handleClick=()=>{
+        funcbool(prevBool => !prevBool)
+    }
+    useEffect(() => {
+        console.log(bool); 
+      }, [bool]);
+    const handleClick1=()=>{
+        console.log(bool)
+    }
+    useEffect(() => {
+        const buttons = document.getElementsByClassName('HeaderButton')
+        Array.from(buttons).forEach((button) => {
+        button.addEventListener('click', handleClick);})
+    
+        return () => {
+            Array.from(buttons).forEach((button) => {
+          button.removeEventListener('click', handleClick1);})
+        };
+      }, []); 
     return(
+        <div>
         <div className="Header">
-            <button>≡</button>
+            <button className="HeaderButton">≡</button>
             <p>AiGenerator</p>
+        </div>
+        {bool ? (
+        <div className="PopUp">
+            <hr/>
+            <button>Neural transfer Paint</button>
+            <hr/>
+            <button>Image Caption generator</button>
+            <hr/>
+            <button>Image ennhancer</button>
+            <hr/>
+            <button>Saved Images</button>
+            <hr/>
+            <button>Log Out</button>
+            <hr/>
+            </div>)
+            :
+            (<div></div>)
+            }
         </div>
     )
 }
